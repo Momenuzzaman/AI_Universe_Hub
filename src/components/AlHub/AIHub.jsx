938c8e56ff06e5bd1d5ae6f40ffd9312dfafee18
mdemon420 <mdeamon4@gmail.com>
2023-05-09 14:45:23 +0600

    use loading

--- src/components/AlHub/AIHub.jsx
+++ src/components/AlHub/AIHub.jsx
@@ -5,6 +5,7 @@ import Button from "../common/Button/Button";
 const AIHub = () => {
   const [tools, setTools] = useState([]);
   const [showAll, setShowAll] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   // load all data
   useEffect(() => {
     const fetchAiData = async () => {
@@ -13,20 +14,29 @@ const AIHub = () => {
       );
       const data = await response.json();
       setTools(data?.data?.tools);
+      setIsLoading(false);
     };
     fetchAiData();
   }, []);
-  console.log(showAll);
   // show all data function
   const handleShowAll = () => {
     setShowAll(!showAll);
-    console.log(showAll);
   };
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center my-72">
+        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400 "></div>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col">
       <Button>Sort By Date</Button>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-3 mx-auto">
-        {tools && tools?.map((tool) => <AiTool key={tool?.id} tool={tool} />)}
+        {tools &&
+          tools
+            .slice(0, showAll ? 12 : 6)
+            ?.map((tool) => <AiTool key={tool?.id} tool={tool} />)}
       </div>
 
       {!showAll && (
