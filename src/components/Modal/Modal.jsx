39dfc5d5edb301fb0322b20cbf029165ded39065
mdemon420 <mdeamon4@gmail.com>
2023-05-09 18:31:02 +0600

    show data in modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 
 const Modal = ({ tool }) => {
+  const { name, image_link } = tool;
   console.log(tool);
   return (
     <div>
       <input type="checkbox" id="tool-modal" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box relative">
+      <div className="modal ">
+        <div className="modal-box w-11/12 max-w-5xl">
           <label
             htmlFor="tool-modal"
             className="btn btn-sm btn-circle absolute right-2 top-2"
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          <div>
+            <div className="hero w-full ">
+              <div className="hero-content flex-col lg:flex-row-reverse">
+                <img
+                  src={image_link && image_link[0]}
+                  className="max-w-sm rounded-lg shadow-2xl"
+                />
+                <div>
+                  <h1 className="text-5xl font-bold">Box Office News!</h1>
+                  <p className="py-6">
+                    Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                    assumenda excepturi exercitationem quasi. In deleniti eaque
+                    aut repudiandae et a id nisi.
+                  </p>
+                  <button className="btn btn-primary">Get Started</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
